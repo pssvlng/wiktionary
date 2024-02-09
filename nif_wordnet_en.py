60b60eacfc6e75bc57c59eb46d385e60b15b3a4e
pssvlng <info@passivlingo.com>
2024-02-09 14:01:14 +0100

    some updates

--- nif_wordnet_en.py
+++ nif_wordnet_en.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 from rdflib import Graph, URIRef, Literal
 from rdflib.namespace import RDF, XSD, SDO
@@ -19,7 +20,7 @@ def get_definition_key(subject):
 
 def get_example_key(subject):
     example_key_cntr[subject] += 1
-    return f'keyword{example_key_cntr[subject]}'
+    return f'example{example_key_cntr[subject]}'
 
 def add_nif_context(g, subject, definition, definition_key, example, example_key, lang):
     sub = URIRef(subject)                       
@@ -59,9 +60,8 @@ def get_annotation_text_example(graph, limit: int, offset: int, db_name:str):
     
     cntr = 0    
     for result in results["results"]["bindings"]:
-        example_key = get_example_key(result["example"]["value"])
+        example_key = get_example_key(result["synset"]["value"])
         add_nif_context(graph, result["synset"]["value"], None, None, result["example"]["value"], example_key, 'en')
-
         cntr += 1
         if (cntr % 1000 == 0):
             print(f'Records processed: {cntr} of {len(results["results"]["bindings"])}')
@@ -77,57 +77,39 @@ def get_annotation_text_def(graph, limit: int, offset: int, db_name:str):
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()    
     
-    cntr = 0    
-    data_to_insert = [(result["synset"]["value"], 
-                        result["def"]["value"])                         
-                        for result in results["results"]["bindings"]]
+    cntr = 0        
     for result in results["results"]["bindings"]:
-        definition_key = get_definition_key(result["def"]["value"])
-        add_nif_context(graph, result["synset"]["value"], result["def"]["value"], definition_key, 'en')
+        definition_key = get_definition_key(result["synset"]["value"])
+        add_nif_context(graph, result["synset"]["value"], result["def"]["value"], definition_key, None, None, 'en')
         cntr += 1
         if (cntr % 1000 == 0):
             print(f'Records processed: {cntr} of {len(results["results"]["bindings"])}')
-    
 
-def get_wordnet_annotation_text(graph, limit: int, offset: int, db_name:str):    
-    sparql = SPARQLWrapper("http://localhost:8890/sparql")
-    query = WORDNET_RDF    
-    query = query.replace('{LIMIT_VAR}', str(limit))
-    query = query.replace('{OFFSET_VAR}', str(offset))
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()    
-    
-    cntr = 0    
-    data_to_insert = [(result["lexEntry"]["value"], 
-                        result["canon"]["value"], 
-                        result["writtenRep"]["value"], 
-                        result["pos"]["value"], 
-                        result["sense"]["value"], 
-                        result["synset"]["value"],    
-                        result["def"]["value"],                                                                                                                                             
-                        result["writtenRep"]["value"])
-                        for result in results["results"]["bindings"]]
-        
-    for item in data_to_insert:
-        print(item)
-    
+    return graph    
+   
 db_name = "wiktionary_en.db"
 limit = 10000
 definition_key_cntr = defaultdict(int)
 example_key_cntr = defaultdict(int)
-graph = Graph()
 
-#for x in range(0,22):    
-#    offset = limit * x
-#    get_wordnet_annotation_text(graph, limit, offset, db_name)    
+if len(sys.argv) == 4:
+    thread_cnt = int(sys.argv[1])
+    thread_nr = int(sys.argv[2])
+    category = sys.argv[3]
 
-for x in range(0,13):    
-    offset = limit * x
-    get_annotation_text_def(graph, limit, offset, db_name)    
+    if category == "definition":
+        #range: 0-13        
+        offset = limit * thread_nr
+        graph = Graph()
+        get_annotation_text_def(graph, limit, offset, db_name)    
+        output_file = f"wordnet_en_definitions_{thread_nr}.ttl"    
+        graph.serialize(destination=output_file, format="turtle", encoding='UTF-8')          
+    
+    if category == "example":
+        #range: 0-6        
+        offset = limit * thread_nr  
+        graph = Graph()
+        get_annotation_text_example(graph, limit, offset, db_name)
+        output_file = f"wordnet_en_examples_{thread_nr}.ttl"    
+        graph.serialize(destination=output_file, format="turtle", encoding='UTF-8')          
 
-for x in range(0,6):  
-    offset = limit * x  
-    get_annotation_text_example(graph, limit, offset, db_name)
-
-#get_wordnet_annotation_text_cnt()
